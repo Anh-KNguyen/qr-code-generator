69521923690720937181c6ea0939718479273f74
Anh Nguyen <anhknguyen97@yahoo.com>
2020-10-30 14:55:22 -0700

    Include instructions

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
+const qr = require('qr-image')
 
-constant generate = async request => {
-  return new Response("Hello worker", {status: 200})
+const generate = async request => {
+  const {text} = await request.json()
+  const qr_svg = qr.imageSync(text || "http://anhknguyen.com", {type: 'svg'})
+  const headers = {"Content-Type": "image/svg+xml"}
+  return new Response(qr_svg, {headers})
 }
+
+const landing = `
+<h1>QR Code Generator</h1>
+<input type="text" id="text" placeholder="Your URL here.."></input>
+<button onclick="generate()">Generate QR</button>
+<p>Find the QR code in the "Network" tab of your browser's developer tools.</p>
+<script>
+  function generate() {
+    fetch(window.location.pathname, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ text: document.querySelector("#text").value })
+    })
+  }
+</script>
+`
 
 async function handleRequest(request) {
   if (request.method === "POST") {
     response = await generate(request)
   } else {
-    response = new Response("Expected POST", {status: 405})
+    response = new Response(landing, { headers: { "Content-Type": "text/html" } })
   }
   return response
 }
+
+addEventListener("fetch", event => {
+  event.respondWith(handleRequest(event.request))
+})
