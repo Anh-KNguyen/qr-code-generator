790136b5de5dd94be902a1118c380884c8d68d26
Anh Nguyen <anhknguyen97@yahoo.com>
2020-11-03 15:52:07 -0800

    Create UI for QR Generator

--- index.js
+++ index.js
@@ -8,10 +8,17 @@ const generate = async request => {
 }
 
 const landing = `
-<h1>QR Code Generator</h1>
-<input type="text" id="text" placeholder="Your URL here.."></input>
-<button onclick="generate()">Generate QR</button>
-<p>Find the QR code in the "Network" tab of your browser's developer tools.</p>
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<div style="text-align:center;padding-top:60px">
+  <h1 class="w3-text-teal" style="font-family:Tahoma, Verdana, Segoe, sans-serif">Generate a QR Code</h1>
+  <div style="padding-top:20px">
+    <input type="text" id="text" style="border-radius:4px;border-color:ghostwhite" placeholder="Your URL here.."></input>
+    <button onclick="generate()" style="background-color: azure;border-radius: 4px;border-color: aliceblue;">Generate QR</button>
+  </div>
+  <body>
+    <div id="myQrCode" style="width:300px;height:300px;display:block;margin:auto;padding-top:100px"></div>
+  </body>
+</div>
 <script>
   function generate() {
     fetch(window.location.pathname, {
@@ -19,6 +26,12 @@ const landing = `
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ text: document.querySelector("#text").value })
     })
+    .then(response => response.text())
+    .then((text) => {
+      let svgStr = text;
+      let svg = document.getElementById('myQrCode');
+      svg.innerHTML = svgStr;
+    });
   }
 </script>
 `
